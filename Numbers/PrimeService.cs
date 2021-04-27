946185eb17afbee1621fe37c27e39021841a5ddc
Tim Heuer <tim@timheuer.com>
2021-04-26 21:05:04 -0700

    Fixing unit test

--- Numbers/PrimeService.cs
+++ Numbers/PrimeService.cs
@@ -9,11 +9,6 @@
                 return false;
             }
 
-            if (candidate > 100)
-            {
-                return false;
-            }
-
             for (int divisor = 2; divisor <= Math.Sqrt(candidate); ++divisor)
             {
                 if (candidate % divisor == 0)
