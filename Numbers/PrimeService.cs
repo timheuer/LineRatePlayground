8991c299e05d2c4de607892487400848d36b7332
Tim Heuer <tim@timheuer.com>
2021-04-26 21:24:18 -0700

    Adding another condition in IsPrime

--- Numbers/PrimeService.cs
+++ Numbers/PrimeService.cs
@@ -9,6 +9,11 @@
                 return false;
             }
 
+            if (candidate > 100)
+            {
+                return false;
+            }
+
             for (int divisor = 2; divisor <= Math.Sqrt(candidate); ++divisor)
             {
                 if (candidate % divisor == 0)
